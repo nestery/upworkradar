b6a27ebb74968d380fa82a7ddefc1eb950968d3c
rsafin <favoritt>
2020-09-18 17:09:03 +0300

    new layout

---
+++ layouts/auth.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="fill-height">
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
