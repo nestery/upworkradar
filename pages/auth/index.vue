fd20bc91efb3c57e24c60485e352cf8a69c6b991
rsafin <favoritt>
2020-09-18 17:09:15 +0300

    auth page started

---
+++ pages/auth/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row align="center" justify="center">
+    <v-col cols="12">
+      <v-text-field outlined></v-text-field>
+      <v-text-field outlined></v-text-field>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  layout: 'auth',
+}
+</script>
+
+<style></style>
